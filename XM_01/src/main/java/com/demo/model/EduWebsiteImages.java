d127b6c85663dcfe94fcf8d0e4f26f9fd6183495
huangxu <978757271@qq.com>
2018-11-29 14:41:02 +0800

    hx

---
+++ XM_01/src/main/java/com/demo/model/EduWebsiteImages.java
@@ -0,0 +1,82 @@
+package com.demo.model;
+
+/**
+ * @author ZHAIYIBAO
+ *	bannerÍ¼¹ÜÀí
+ */
+public class EduWebsiteImages extends PageBean {
+
+	private Integer image_id;	//
+	private String image_url;	//Í¼Æ¬µØÖ·
+	private String link_address;	//Í¼Á´½ÓµØÖ·
+	private String title;		//Í¼±êÌâ
+	private Integer type_id;	//Í¼Æ¬ÀàĞÍ
+	private Integer series_number;	//ĞòÁĞºÅ
+	private String preview_url;	//ÂÔËõÍ¼Æ¬µØÖ·
+	private String color;		//±³¾°É«
+	private String describe;	//Í¼Æ¬ÃèÊö 
+	public Integer getImage_id() {
+		return image_id;
+	}
+	public void setImage_id(Integer image_id) {
+		this.image_id = image_id;
+	}
+	public String getImage_url() {
+		return image_url;
+	}
+	public void setImage_url(String image_url) {
+		this.image_url = image_url;
+	}
+	public String getLink_address() {
+		return link_address;
+	}
+	public void setLink_address(String link_address) {
+		this.link_address = link_address;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Integer getType_id() {
+		return type_id;
+	}
+	public void setType_id(Integer type_id) {
+		this.type_id = type_id;
+	}
+	public Integer getSeries_number() {
+		return series_number;
+	}
+	public void setSeries_number(Integer series_number) {
+		this.series_number = series_number;
+	}
+	public String getPreview_url() {
+		return preview_url;
+	}
+	public void setPreview_url(String preview_url) {
+		this.preview_url = preview_url;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getDescribe() {
+		return describe;
+	}
+	public void setDescribe(String describe) {
+		this.describe = describe;
+	}
+	@Override
+	public String toString() {
+		return "EduWebsiteImages [image_id=" + image_id + ", image_url=" + image_url + ", link_address=" + link_address
+				+ ", title=" + title + ", type_id=" + type_id + ", series_number=" + series_number + ", preview_url="
+				+ preview_url + ", color=" + color + ", describe=" + describe + "]";
+	}
+	
+	
+	
+
+}
